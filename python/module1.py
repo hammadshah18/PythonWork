1aa85d57b220b392230bbb70900cb56180d065d6
hammadshah18 <hammadshah7218@gmail.com>
2024-11-27 18:59:09 +0500

    module1

---
+++ python/module1.py
@@ -0,0 +1,19 @@
+# //module 1 question
+a=12
+b=3
+c=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a/c)
+
+
+# // module 2 question
+h="Hammad"
+print(h)
+print(h[4])
+print(h[1:5])
+print(h*5,end=" ")
+print(h +""+"shah")

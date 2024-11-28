2bc9c447755b3f1f386b1c3d9ef4b438bae0b34d
hammadshah18 <hammadshah7218@gmail.com>
2024-11-28 14:05:41 +0500

    loop

---
+++ python/module2.py
@@ -0,0 +1,11 @@
+# question no 1
+for a in range(1,11,1) :
+    print(a)
+    # Question No 2
+a=1
+while a<11: 
+        print(a)
+        a=a+1
+    # question 3
+for a in range(0,11,2) :
+   print(a)

e462040bc3f4eea76e3a086f6cdbcd9a2db906c3
hammadshah18 <hammadshah7218@gmail.com>
2024-11-28 13:16:28 +0500

    ptip module 1

--- python/module1.py
+++ python/module1.py
@@ -9,11 +9,37 @@ print(a/b)
 print(a%b)
 print(a//b)#floor division
 
-
-# // module 2 question
+# module 2 question
 h="Hammad"
 print(h)
+r="raza"
+name = h+r
+print(name)
 print(h[4])
 print(h[1:5])
 print(h*5,end=" ")
 print(h +""+"shah")
+# question NO 3
+# a=[]
+a=[1,2,3,5,4]
+a.remove(2)
+print(a)
+a.append(4)
+print(a)
+a.insert(2,3)
+print(a)
+
+
+
+# question NO 4
+import math
+a=input("enter your number ")
+# print(a)
+a=int(a)
+
+print(math.sqrt(a))
+print(math.pow(a,2))
+print(math.log10(a))
+print(math.sin(math.radians(a)))
+print(math.cos(math(a)))
+print(type(a))

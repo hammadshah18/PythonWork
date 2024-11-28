6ec01c9b2f412850f9f82a04fcdaf4573e5c3d69
hammadshah18 <hammadshah7218@gmail.com>
2024-11-28 11:20:26 +0500

    modify

--- python/task.py
+++ python/task.py
@@ -1,1 +1,13 @@
-print("hammad","is","very","smart",sep='-')
+
+print(a,b,c)
+a=b=c=3
+print(a,b,c)
+input("your Name :")
+# taking user input from here
+a=input("enter you number :")
+b=input("enter your second number :")
+print(type(a),type(b)) #this type keyword tells us about data type of value
+
+c= int(a)+int(b) #this is type casting we converted string data type into int data type
+print(c)
+print(type(a))# data will be same as old 

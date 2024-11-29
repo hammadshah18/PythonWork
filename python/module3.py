b20499ed6f56f2e7bd815d1013dcf05a4f7919f2
hammadshah18 <hammadshah7218@gmail.com>
2024-11-29 20:57:40 +0500

    module2question

---
+++ python/module3.py
@@ -0,0 +1,14 @@
+# a=10
+# if(a<0) :
+#     print("a is a nagative number ")
+# elif(a>0):
+#     print("a is a positive number")
+# else:
+#     print("a is zero ")
+
+a=12
+b=13
+if(a<b):
+    print("a is samller than b")
+else:
+    print("a is greater than b")

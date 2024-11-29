2c2d9366193da16faf709f217e6464cf3db7c124
hammadshah18 <hammadshah7218@gmail.com>
2024-11-29 23:27:08 +0500

    odd

--- python/loop.py
+++ python/loop.py
@@ -1,5 +1,11 @@
-shah=["red" ,"blue","green","white"]
-for syed in shah :
-  print(syed)
-  for i in syed:
-   print(i)
+# shah=["red" ,"blue","green","white"]
+# for syed in shah :
+#   print(syed)
+#   for i in syed:
+#    print(i)
+# sum=0
+# for i in range(1,6,1):
+#     sum=sum+i
+#     print(sum)
+for i in range(1,12,2):
+    print(i)
--- python/module2.py
+++ python/module2.py
@@ -1,14 +1,18 @@
 # question no 1
-for a in range(1,11,1) :
-    print(a)
+# for a in range(1,11,1) :
+#     print(a)
     # Question No 2
-a=1
-while a<11: 
-        print(a)
-        a=a+1
-    # question 3
-for a in range(0,11,2) :
-   print(a)
+# a=1
+# while a<11: 
+#         print(a)
+#         a=a+1
+#     # question 3
+# for a in range(0,11,2) :
+#    print(a)
+# question 4
+# for i in range(1,12,2):
+#     print(i)
+#question 5
    # a=10
 # if(a<0) :
 #     print("a is a nagative number ")
@@ -16,10 +20,15 @@ for a in range(0,11,2) :
 #     print("a is a positive number")
 # else:
 #     print("a is zero ")
-
-a=12
-b=13
-if(a<b):
-    print("a is samller than b")
-else:
-    print("a is greater than b")
+# question 6
+# a=12
+# b=13
+# if(a<b):
+#     print("a is samller than b")
+# else:
+#     print("a is greater than b")
+# question 7
+#     sum=0
+# for i in range(1,6,1):
+#     sum=sum+i
+#     print(sum)
